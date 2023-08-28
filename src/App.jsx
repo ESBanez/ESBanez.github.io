72e6f2f76ac1fa8e3f59b4cbdeebdcd1faefe1df
Ederick S. Banez <esb.tailor@gmail.com>
2023-08-29 00:18:33 +0800

    added pages and react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
-function App() {
-  return <>Hello</>;
-}
+import React from "react"; // Make sure to import React
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Projects from "./pages/Projects.jsx";
+import Skills from "./pages/Skills.jsx";
 
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/skills" element={<Skills />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
 export default App;
