711adb6dd64ec439bb908733a67ce7540e2a0897
Ederick S. Banez <esb.tailor@gmail.com>
2023-10-25 14:17:16 +0800

    fixed the path to Navbar pages/ paths

---
+++ src/Theme.jsx
@@ -0,0 +1,9 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#040D12', // Replace '#f00' with your desired color value
+    },
+  },
+})
